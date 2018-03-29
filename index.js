2937c42057b22dfb6fd5a1d59bb7940b9b8bcb22
maz <complex.logik@gmail.com>
2018-03-28 23:23:06 -0400

    fixed up some more issures with symantics

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 
 
-let pageChange = document.querySelector('#nav1');
+let msg = document.querySelector('#nav1');
 
-pageChange.addEventListener('click',(event) => {
+msg.addEventListener('click',(event) => {
     // let hideOne = document.querySelector('#imgOne');
     // hideOne.setAttribute('class', 'hidden');
-    location.reload();
+    alert(`Sorry we don't really sell anything.... We are a fictatious company.`);
 })
+
 
